470d1ad01a0963b4231ff4558eb1d1a64e1a5614
songhaopeng <jsong2018@icloud.com>
2020-09-02 16:03:04 +0800

    version 2

--- LinkedArrayBag/LinkedBag.cpp
+++ LinkedArrayBag/LinkedBag.cpp
@@ -52,44 +52,94 @@ int LinkedBag<ItemType>::getCurrentSize() const {
 	return itemCount;
 }
 
+// the copy constructor
+// it makes a copy of an object, it is invoked implicitly when:
+// 1) use the assignment operator to assign an object to a variable
+// 2) pass an object to a function by value
+// 3) return the object from a valued function
+// 4) define and initialize an object as in:
+//	LinkedBag bag2(bag1), where bag1 exists already
+// if not using copy constructor, only shallow copy is used
 template<class ItemType>
 LinkedBag<ItemType>::LinkedBag(const LinkedBag& target) {
 
 }
 
+// must have a destructor if class allocates memory dynamically
 template<class ItemType>
 LinkedBag<ItemType>::~LinkedBag() {
-
+	clear();
 }
 
+// deletes one occurrence of a given entry, if the entry exists, replace the entry with with entry in the 
+// first node, then delete the first node
+// any time use 'new' to allocate memory, the memory must eventually be deallocated by 'delete'
 template<class ItemType>
 bool LinkedBag<ItemType>::remove(const ItemType& entry) {
-	return false;
+	Node<ItemType>* entryNodeptr = getPointerTo(entry);
+	bool canRemove = !isEmpty() && entryNodeptr != nullptr;
+	if (canRemove) {
+		// copy data from first node to located node
+		entryNodeptr -> setItem(headPtr -> getItem());
+		// disconnect the first node
+		Node<ItemType>* nodeToDeletePtr = headPtr;
+		headPtr = headPtr -> getNext();
+		// return node to the system
+		// so system can resuse the memory
+		nodeToDeletePtr -> setNext(nullptr);
+		delete nodeToDeletePtr;
+		nodeToDeletePtr = nullptr;
+		itemCount--;
+	}
+	return canRemove;
 }
 
 template<class ItemType>
 void LinkedBag<ItemType>::clear() {
-	return;
+	Node<ItemType>* nodeToDeletePtr = headPtr;
+	while (headPtr != nullptr) {
+		headPtr = headPtr -> getNext();
+		nodeToDeletePtr -> setNext(nullptr);
+		delete nodeToDeletePtr;
+		nodeToDeletePtr = headPtr;
+	} 
+	// headPtr is nullptr; nodeToDeletePtr is nullptr
+	itemCount = 0;
 }
 
 template<class ItemType>
 bool LinkedBag<ItemType>::contains(const ItemType& entry) const {
-	return false;
+	return getPointerTo(entry) != nullptr;
 }
 
 template<class ItemType>
 int LinkedBag<ItemType>::getFrequencyOf(const ItemType& entry) const {
-	return 1;
+	int freq = 0;
+	int counter = 0;
+	Node<ItemType>* curr = headPtr;
+	while (curr != nullptr && counter < itemCount) {
+		if (entry == curr -> getItem()) {
+			freq++;
+		}
+		counter++;
+		curr = curr -> getNext()
+	}
+	return freq;
 }
 
-
-
-
-
-
-
-
-
+template<class ItemType>
+Node<ItemType> LinkedBag<ItemType>::getPointerTo(const ItemType& entry) const {
+	bool found = false;
+	Node<ItemType>* curr = headPtr;
+	while (!found && curr != nullptr) {
+		if (entry == curr -> getItem()) {
+			found = true;
+		} else {
+			curr = curr -> getNext();
+		}
+	}
+	return curr;
+}
 
 
 
