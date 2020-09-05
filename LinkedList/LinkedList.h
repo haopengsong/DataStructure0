6a98944c204dc22f8aa1db15e8cb8d7d27b1c199
songhaopeng <jsong2018@icloud.com>
2020-09-05 12:57:38 +0800

    finished ll.h

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -13,5 +13,26 @@ impose a fixed length on the list (however, does limit by system storage)
 template<class ItemType>
 class LinkedList : public ListInterface<ItemType> {
 private:
+	Node<ItemType>* headPtr; // pointer to first node
+	int itemCount; // current element count of list
 	
-}
+	// locates a specified node in a linked list
+	Node<ItemType>* getNodeAt(int position) const;
+public:
+	LinkedList();
+	LinkedList(const LinkedList<ItemType>& aList);
+	virtual ~LinkedList();
+
+	bool isEmpty() const;
+	int getLength() const;
+	bool insert(int newPosition, const ItemType& newEntry);
+	bool remove(int position);
+	void clear();
+
+	// @throw PrecondViolatedExcept if position < 1 or position > getLength()
+	ItemType getEntry(int position) const throw(PreCondViolatedExcept);
+	ItemType replace(int position, const ItemType& newEntry) throw(PreCondViolatedExcept)
+
+};
+#include "LinkedList.cpp"
+#endif
