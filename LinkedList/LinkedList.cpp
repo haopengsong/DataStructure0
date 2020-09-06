28fc58c9cb184fdb7eb0a2fab0ba468739cb6ad6
songhaopeng <jsong2018@icloud.com>
2020-09-06 19:11:14 +0800

    ll finished

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -60,35 +60,102 @@ bool LinkedList<ItemType>::insert(int newPosition, const ItemType& newEntry) {
 			newNodePtr -> setNext(prePtr -> getNext());
 			prePtr -> setNext(newNodePtr);
 		}
+		itemCount++;
 	}
 	return canInsert;
 }
 
+
+// 1. locate the node that you want to remove
+// 2. disconnect this node from the linked chain by changing pointers
+// 3. return the node to the system
 template<class ItemType>
 bool LinkedList<ItemType>::remove(int position) {
-
+	bool canRemove = (position >= 1 && position <= itemCount);
+	if (canRemove) {
+		Node<ItemType>* curPtr = nullptr;
+		if (position == 1) {
+			// remove the first node
+			curPtr = headPtr;
+			headPtr = headPtr -> getNext();
+ 		} else {
+ 			// find the node that is before the one to remove
+ 			Node<ItemType>* prev = getNodeAt(position - 1);
+ 			// point to node to remove
+ 			curPtr = prev -> getNext();
+ 			// disconnect indicated node from chain by connecting the prior
+ 			// node with the one after
+ 			prev -> setNext(curPtr -> getNext());
+ 		}
+ 		curPtr -> setNext(nullptr);
+ 		delete curPtr;
+ 		curPtr = nullptr;
+ 		itemCount--;
+	}
+	return canRemove;
 }
 
+// invoke remove(1) repeatedly until the list is empty
 template<class ItemType>
 void LinkedList<ItemType>::clear() {
-
+	while (!isEmpty()) {
+		remove(1);
+	}
 }
 
 template<class ItemType>
 LinkedList<ItemType>::~LinkedList() {
-
+	clear();
 }
 
 template<class ItemType>
-LinkedList<ItemType>::LinkedList(const LinkedList<ItemType>& aList) {
-	
+LinkedList<ItemType>::LinkedList(const LinkedList<ItemType>& rhs) {
+	itemCount = rhs.itemCount;
+	Node<ItemType>* originalChainPtr = rhs.headPtr;
+	if (originalChainPtr == nullptr) {
+		// empty so is the copy
+		headPtr = nullptr;
+	} else {
+		// copy head node
+		headPtr = new Node<ItemType>();
+		headPtr -> setItem(originalChainPtr -> getItem());
+		// copy rest nodes
+		Node<ItemType>* newChainPtr = headPtr;
+		originalChainPtr = originalChainPtr -> getNext();
+		while (originalChainPtr != nullptr) {
+			// get and copy the next item
+			ItemType nextItem = originalChainPtr -> getItem();
+			// create a node contains the next item
+			Node<ItemType>* copy = new Node<ItemType>(nextItem);
+			// link
+			newChainPtr -> setNext(copy);
+			// advance
+			newChainPtr = newChainPtr -> getNext();
+			originalChainPtr = originalChainPtr -> getNext();
+		}
+		newChainPtr -> setNext(nullptr);
+	}
 }
 
+template<class ItemType>
+bool LinkedList<ItemType>::isEmpty() {
+	return itemCount == 0;
+}
 
-
-
-
-
+template<class ItemType>
+ItemType LinkedList<ItemType>::replace(int position, const ItemType& newEntry)
+throw(PrecondViolatedExcept) {
+	bool canReplace = (position >= 1 && position <= itemCount);
+	if (canReplace) {
+		Node<ItemType>* NodeToReplace = getNodeAt(position);
+		NodeToReplace -> setItem(newEntry);
+		return NodeToReplace -> getItem();
+	} else {
+		std::string message = "replace() called with an empty list or ";
+		message += "invalid position";
+		throw(PrecondViolatedExcept(message));
+	}
+}
 
 
 
