633aa0a36a8deee064e3594b791c297c6cbed86d
haopengsong <jsong2018@icloud.com>
2020-12-29 23:33:33 +0800

    getNodeBefore

--- SortedList/LinkedSortedList.cpp
+++ SortedList/LinkedSortedList.cpp
@@ -7,4 +7,41 @@ template<class ItemType>
 LinkedSortedList<ItemType>::LinkedSortedList(const LinkedSortedList<ItemType>& alist) {
 	// copy constructor
 	// non-recursive approach
+
+	// recursive approach
+	headPtr = copyChain(alist.headPtr);
+	itemCount = alist.itemCount;
 }
+
+
+/*
+Begin by testing the pointer argument, if it contains nullptr, the original sorted list is 
+empty, so the method returns nullptr. Otherwise, the method creates a new node containing the 
+data from the first node of the given chain. The method then recursively inserts the new node 
+into the copy of the chain. After each insertion, the method returns a pointer to the new chain
+*/
+template<class ItemType>
+auto LinkedSortedList<ItemType>::copyChain(const std::shared_ptr<Node<ItemType>>& origChainPtr) {
+	std::shared_ptr<Node<ItemType>> copiedChainPtr;
+	// initial value is not nullptr
+	if (origChainPtr != nullptr) {
+		// build new chain from given one
+		// create new node
+		copiedChainPtr = std::make_shared<Node<ItemType>>(origChainPtr -> getItem());
+		copiedChainPtr -> setNext(copyChain(origChainPtr -> getNext()));
+	}
+	return copiedChainPtr;
+}
+
+template<class ItemType>
+void LinkedSortedList<ItemType>::insertSorted(const ItemType& entry) {
+
+}
+
+// the performance of insertSorted depends on the efficiency of the method getNodeBefore
+// need a pointer to the current node so we can [[[compare]]] its entry to the desired entry
+template<class ItemType>
+auto LinkedSortedList<ItemType>::getNodeBefore(const ItemType& entry) const {
+
+}
+
