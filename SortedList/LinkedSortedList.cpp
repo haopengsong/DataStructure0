fb47ac35a5897ccd067e45f350fd75e0eb84ff8d
haopengsong <jsong2018@icloud.com>
2021-01-02 22:40:59 +0800

    lsl

--- SortedList/LinkedSortedList.cpp
+++ SortedList/LinkedSortedList.cpp
@@ -1,3 +1,5 @@
+#include "LinkedSortedList.h"
+
 template<class ItemType>
 LinkedSortedList<ItemType>::LinkedSortedList() : headPtr(nullptr), itemCount(0) {
 	// default constructor
@@ -11,6 +13,11 @@ LinkedSortedList<ItemType>::LinkedSortedList(const LinkedSortedList<ItemType>& a
 	// recursive approach
 	headPtr = copyChain(alist.headPtr);
 	itemCount = alist.itemCount;
+}
+
+template<class ItemType>
+auto LinkedSortedList<ItemType>::getNodeAt(int position) const {
+
 }
 
 
@@ -59,8 +66,79 @@ bool LinkedSortedList<ItemType>::insertSorted(const ItemType& newEntry) {
 
 // the performance of insertSorted depends on the efficiency of the method getNodeBefore
 // need a pointer to the current node so we can [[[compare]]] its entry to the desired entry
+// also must retain a pointer to the previous node
 template<class ItemType>
 auto LinkedSortedList<ItemType>::getNodeBefore(const ItemType& entry) const {
+	auto curPtr = headPtr;
+	std::shared_ptr<Node<ItemType>> prevPtr;
+	while (curPtr != nullptr && entry > curPtr -> getItem()) {
+		prevPtr = curPtr;
+		curPtr = curPtr -> getNext();
+	}
+	return prevPtr;
+}
+
+template<class ItemType>
+bool LinkedSortedList<ItemType>::removeSorted(const ItemType& entry) {
 
 }
+
+template<class ItemType>
+bool LinkedSortedList<ItemType>::getPosition(const ItemType& entry) {
+
+}
+
+
+template<class ItemType>
+bool LinkedSortedList<ItemType>::isEmpty() const {
+
+}
+
+template<class ItemType>
+int LinkedSortedList<ItemType>::getLength() const {
+
+}
+
+template<class ItemType>
+bool LinkedSortedList<ItemType>::remove(int position) {
+
+}
+
+template<class ItemType>
+void LinkedSortedList<ItemType>::clear() {
+
+}
+
+template<class ItemType>
+ItemType LinkedSortedList<ItemType>::getEntry(int position) const 
+throw(PrecondViolatedExcept) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ SortedList/Node.cpp
@@ -0,0 +1,79 @@
+/** Node.cpp 
+#include "Node.h"
+
+template<class ItemType>
+Node<ItemType>::Node() : next(nullptr) {
+	// default constructor
+}
+
+template<class ItemType>
+Node<ItemType>::Node(const ItemType& item) : item(item), next(nullptr) {
+	// constructor
+}
+
+template<class ItemType>
+Node<ItemType>::Node(const ItemType& item, Node<ItemType>* nextNodePtr) : 
+item(item), next(nextNodePtr) {
+	// construtor
+}
+
+template<class ItemType>
+void Node<ItemType>::setItem(const ItemType& item) {
+	this -> item = item;
+}
+
+template<class ItemType>
+void Node<ItemType>::setNext(Node<ItemType>* nextNodePtr) {
+	this -> next = nextNodePtr;
+}
+
+template<class ItemType>
+ItemType Node<ItemType>::getItem() const {
+	return this -> item;
+}
+
+template<class ItemType>
+Node<ItemType>* Node<ItemType>::getNext() const {
+	return this -> next;
+}
+*/
+
+/** Node.cpp */
+// version 2, using shared_ptr
+#include "Node.h"
+
+template<class ItemType>
+Node<ItemType>::Node() : next(nullptr) {
+	// default constructor
+}
+
+template<class ItemType>
+Node<ItemType>::Node(const ItemType& item) : item(item), next(nullptr) {
+	// constructor
+}
+
+template<class ItemType>
+Node<ItemType>::Node(const ItemType& item, std::shared_ptr<Node<ItemType>> nextNodePtr) : 
+item(item), next(nextNodePtr) {
+	// construtor
+}
+
+template<class ItemType>
+void Node<ItemType>::setItem(const ItemType& item) {
+	this -> item = item;
+}
+
+template<class ItemType>
+void Node<ItemType>::setNext(std::shared_ptr<Node<ItemType>> nextNodePtr) {
+	this -> next = nextNodePtr;
+}
+
+template<class ItemType>
+ItemType Node<ItemType>::getItem() const {
+	return this -> item;
+}
+
+template<class ItemType>
+auto Node<ItemType>::getNext() const {
+	return this -> next;
+}
