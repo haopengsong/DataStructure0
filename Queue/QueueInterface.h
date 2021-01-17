cc3dfe5c1348589af28abb3e45c713082a4fc111
haopengsong <jsong2018@icloud.com>
2021-01-17 21:10:06 +0800

    pq

--- Queue/QueueInterface.h
+++ Queue/QueueInterface.h
@@ -1,6 +1,15 @@
 /*
 1) Chapter 13-1
 2) C++ interface for queues
+3) Simple queue applications:
+	- queue can retain characters in the order in which you type them
+	- recognizing Palindromes:
+		- stack for reverse the order of occurrences
+		- queue to preserve the order of occurrences
+		- compare
+4) Simulation: a technique for modeling the behavior of both natural and human-made systems.
+	- The goal of a simulation is to generate statistics that summarize the performance of an 
+	existing system or to predict the performance of a proposed system.  
 */
 
 #ifndef QUEUE_INTERFACE_
