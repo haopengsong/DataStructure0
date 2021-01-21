dd6e6b90a5c95bcb295d3e931443d1fb179f063f
haopengsong <jsong2018@icloud.com>
2021-01-21 23:47:34 +0800

    linked queue

---
+++ Queue/LinkedQueue/LinkedQueue.h
@@ -0,0 +1,21 @@
+/* ADT queue: Linked-based implementation
+A chain of linked nodes with head and tail pointers
+*/
+
+#ifndef LINKED_QUEUE_
+#define LINKED_QUEUE_
+
+#include "QueueInterface.h"
+#include "Node.h"
+#include "PrecondViolatedExcept.h"
+#include <memory>
+
+template<class ItemType>
+class LinkedQueue : public QueueInterface<ItemType>
+{
+private:	
+	
+public:
+
+};
+
