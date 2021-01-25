aaec48fde4b77e1ece45316ba9a38c01d3e04dc6
haopengsong <jsong2018@icloud.com>
2021-01-25 22:51:25 +0800

    dequeue with smart pointer

---
+++ Queue/LinkedQueue/LinkedQueue.cpp
@@ -0,0 +1,43 @@
+#include "LinkedQueue.h"
+
+template<class ItemType>
+bool LinkedQueue<ItemType>::enqueue(const ItemType& entry) {
+	std::shared_ptr<Node<ItemType>> newNodePtr = std::make_shared<Node<ItemType>>(entry);
+	
+	// if queue is empty
+	// both the front/back pointers point to the newNode
+	if (isEmpty()) {
+		frontPtr = newNodePtr; 
+	} else {
+		backPtr -> setNext(newNodePtr); // for queue not empty
+	}
+	backPtr = newNodePtr;
+	return true;
+}
+
+
+// Remove from a queue of one item is a special case that sets the external pointers
+// backPtr and frontPtr to nullptr
+template<class ItemType>
+bool LinkedQueue<ItemType>::dequeue() {
+	bool result = false;
+	if (!isEmpty()) {
+		// not empty, remove front
+		std::shared_ptr<Node<ItemType>> nodeToDeletePtr = frontPtr;
+		if (frontPtr == backPtr) {
+			// special case: one node in queue
+			// set frontPtr and backPtr to nullptr
+			frontPtr.reset();
+			backPtr.reset();
+		} else {
+			frontPtr = frontPtr -> getNext();
+			
+			// maintain an accurate reference count for first node 
+			nodeToDeletePtr -> setNext(nullptr);
+			
+			// removed node will be deallocated when method ends
+			result = true;
+		}
+	}
+	return result;
+}
--- Queue/LinkedQueue/LinkedQueue.h
+++ Queue/LinkedQueue/LinkedQueue.h
@@ -14,8 +14,21 @@ template<class ItemType>
 class LinkedQueue : public QueueInterface<ItemType>
 {
 private:	
-	
+	// with two pointers: head, tail
+	std::shared_ptr<Node<ItemType>> frontPtr;
+	std::shared_ptr<Node<Itemtype>> backPtr;	
 public:
-
+	LinkedQueue();
+	LinkedQueue(const LinkedQueue& q);
+	~LinkedQueue();
+	
+	bool isEmpty() const;
+	bool enqueue(const ItemType& entry);
+	bool dequeue();
+	
+	ItemType peekFront() const
+	throw(PrecondViolatedExcept);
 };
+#include "LinkedQueue.cpp"
+#endif
 
