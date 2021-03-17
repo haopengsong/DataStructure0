c9089ff073abd455ac33cb45171ae66098dada10
haopengsong <jsong2018@icloud.com>
2021-03-17 23:45:13 +0800

    binary tree, interface

--- BinaryTree/BinaryTreeINterface.h
+++ BinaryTree/BinaryTreeINterface.h
@@ -1,4 +1,27 @@
 /** Interface for the ADT binary tree **/
 
 #ifndef BINARY_TREE_INTERFACE_
-#define 
+#define BINARY_TREE_INTERFACE_
+#include "NotFoundException.h"
+
+template<class ItemType>
+class BinaryTreeInterface {
+public:
+    // whether the tree is empty 
+    virtual bool isEmpty() const = 0;
+
+    // gets the height of this binary tree
+    virtual int getHeight() const = 0;
+
+    // gets the number of nodes in this binary tree
+    virtual int getNumberOfNodes() const = 0;
+
+    // gets the data that is in the root of this binary tree
+    // 1) the binary tree is not empty. 2) the root's data has been returned, the binary tree is unchanged
+    virtual ItemType getRootData() const = 0;
+
+    // replaces the data in the root of this binary tree with the given data, if the tree is not empty
+    // if the tree is empty, inserts a new root node containing the given data into the tree
+    virtual void setRootData(const ItemType& newData) = 0;
+	
+} 
