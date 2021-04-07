c97a25d44f7907f720f4f708e22f2505afbddef1
haopengsong <jsong2018@icloud.com>
2021-04-07 22:23:20 +0800

    binary tree interface

--- BinaryTree/BinaryTreeINterface.h
+++ BinaryTree/BinaryTreeINterface.h
@@ -23,5 +23,28 @@ public:
     // replaces the data in the root of this binary tree with the given data, if the tree is not empty
     // if the tree is empty, inserts a new root node containing the given data into the tree
     virtual void setRootData(const ItemType& newData) = 0;
-	
-} 
+    
+    // adds the given data to this binary tree
+    virtual bool add(const ItemType& newData) = 0;
+
+    // removes the specified data from this binary tree
+    virtual bool remove(const ItemType& target) = 0;
+
+    // removes all data from this binary tree
+    virtual void clear() = 0;
+    
+    // retrieves the specified data from this binary tree
+    virtual ItemType getEntry(const ItemType& target) const = 0;
+
+    // tests whether the specified data occurs in this binary tree
+    virtual bool contains(const ItemType& target) const = 0;
+
+    // traverses this binary tree in preorder, inorder, postorder and calls the function visit once for each node
+    virtual void preorderTraverse(void visit(ItemType&)) const = 0;
+    virtual void inorderTraverse(void visit(ItemType&)) const = 0;
+    virtual void postorderTraverse(void visit(ItemType&)) const = 0;
+
+    // destroys this tree and frees its assigned memory
+    virtual ~BinaryTreeInterface() { }
+};
+#endif
