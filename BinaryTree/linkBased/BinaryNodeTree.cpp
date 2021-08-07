f18c7ee1e13b3234f7f86c0f0b1e448397b2130c
haopengsong <jsong2018@icloud.com>
2021-08-07 20:59:45 +0800

    implemented some functions for BinaryNodeTree

---
+++ BinaryTree/linkBased/BinaryNodeTree.cpp
@@ -0,0 +1,101 @@
+// constructors
+template<class ItemType>
+BinaryNodeTree<ItemType>::BinaryNodeTree() : rootPtr(nullptr)
+{
+
+}
+
+template<class ItemType>
+BinaryNodeTree<ItemType>::BinaryNodeTree(const ItemType& rootItem)
+    : rootPtr(std::make_shared<BinaryNode<ItemType>>(rootItem, nullptr, nullptr))
+{
+
+}
+
+template<class ItemType>
+BinaryNodeTree<ItemType>::BinaryNodeTree(const ItemType& rootItem,
+                                        const std::shared_ptr<BinaryNodeTree<ItemType>> leftTreePtr,
+                                        const std::shared_ptr<BinaryNodeTree<ItemType>> rightTreePtr)
+    : rootPtr(std::make_shared<BinaryNode<ItemType>>(rootItem, copyTree(leftTreePtr->rootPtr),
+                                                               copyTree(rightTreePtr->rootPtr))
+{
+
+}
+
+// copy constructor
+template<class ItemType>
+BinaryNodeTree<ItemType>::BinaryNodeTree(const BinaryNodeTree<ItemType>& treePtr) 
+{
+    rootPtr = copyTree(treePtr.rootPtr); 
+}
+
+// copy tree using PREORDER traversal
+template<class ItemType>
+std::shared_ptr<BinaryNode<ItemType>> BinaryNodeTree<ItemType>>::copyTree(const std::shared_ptr<BinaryNode<ItemType>> oldTreeRootPtr) const
+{
+    std::shared_ptr<BinaryNode<ItemType>> newRoot;
+    // copy tree node during preorder traversal
+    
+    if (oldTreeRootPtr != nullptr) {
+        // copy node
+        newRoot = std::make_shared<BinaryNode<ItemType>>(oldTreeRootPtr->getItem(),
+                                                        nullptr, nullptr);
+        newRoot->setLeftChildPtr(copyTree(oldTreeRootPtr->getLeftChildPtr()));
+        newRoot->setLeftChildPtr(copyTree(oldTreeRootPtr->getRightChildPtr()));
+    }
+    return newRoot;
+}
+
+// delete tree using POSTORDER traversal
+// because you can delete a node only after you have first traversed and deleted both of its subtrees
+template<class ItemType>
+void BinaryNodeTree<ItemType>::destroyTree(std::shared_ptr<BinaryNode<ItemType>> subTreePtr)
+{
+    if (subTreePtr != nullptr) {
+        destroyTree(subTreePtr->getLeftChildPtr());
+        destroyTree(subTreePtr->getRightChildPtr());
+        subTreePtr.reset();     // Decrement reference count to node
+    }
+}
+
+template<class ItemType>
+int BinaryNodeTree<ItemType>::getHeight() const 
+{
+    return getHeightHelper(rootPtr);
+}
+
+template<class ItemType>
+int BinaryNodeTree<ItemType>::getHeightHelper(std::shared_ptr<BinaryNode<ItemType>> subTreePtr) const
+{
+    if (subTreePtr == nullptr) {
+        return 0;
+    } else {
+        return 1 + std::max(getHeightHelper(subTreePtr->getLeftChildPtr()),
+                            getHeightHelper(subTreePtr->getRightChildPtr()));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
